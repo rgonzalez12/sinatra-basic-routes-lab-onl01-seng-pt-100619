bd7ca608a8396b8d95b0777d34ac2e0a29040e30
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-17 05:25:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "My name is Ray G."
+    "My name is: Ray G."
   end
   
-   
+  get '/hometown' do
+    "My hometown is: Clifton, NJ"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is: Never Gonna Give You Up"
+  end
+  
 end
